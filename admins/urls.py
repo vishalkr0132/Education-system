be9f6e9ad3f82c2ea67de21b59d7f856bc5cd4fe
vishal kumar <vishalkumar.itcs@gmail.com>
2024-02-27 20:24:01 +0530

    updated

--- admins/urls.py
+++ admins/urls.py
@@ -2,7 +2,6 @@ from admins import views
 from django.conf import settings
 from django.urls import include,path
 from .views import logout_view
-from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
